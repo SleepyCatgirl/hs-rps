da29f34616230d824877ec660954d0888ff15487
kurisu <shoukaku00@gmail.com>
2021-12-07 01:43:28 +0000

    Keep track of wins/losses/draws

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Rock Paper Scissors in Haskell
 # To-do
 - Keep playing until you write something else than Rock Paper Scissors [DONE]
 - Reduce repetition in code [DONE]
-- Keep track of Wins/Losses/Draws in a text file
+- Keep track of Wins/Losses/Draws in a text file [DONE]
--- rock.hs
+++ rock.hs
@@ -24,14 +24,24 @@ handOpponent = rollNum >>=
     1 -> return Rock
     2 -> return Paper
     3 -> return Scissors
--- Function that takes Hand as argument, calls handOpponent, and compares your choice with opponent's
-handCompare :: Hand -> IO ()
-handCompare choice =
-  handOpponent >>= (\x -> let enemyStr = "Enemy's hand is: " ++ show x in
-                      case compare choice x of
-                                        EQ -> putStrLn (enemyStr ++ ", Draw")
-                                        LT -> putStrLn (enemyStr ++ ", Loss")
-                                        GT -> putStrLn (enemyStr ++ ", Win"))
+-- Function that takes Hand as argument, and enemy roll , and compares your choice with opponent's
+--      and then subsequently use it
+handCompare :: Hand -> Hand -> IO ()
+handCompare choice opChoice =
+  let enemyStr = "Enemy's hand is: " ++ show opChoice in
+    case compare choice opChoice  of
+      EQ -> putStrLn (enemyStr ++ ", Draw")
+      LT -> putStrLn (enemyStr ++ ", Loss")
+      GT -> putStrLn (enemyStr ++ ", Win")
+-- Write score
+writeHandCompare choice opChoice =
+  let write = appendFile "Score.txt" in
+    case compare choice opChoice  of
+      EQ -> write "Draw\n"
+      LT -> write "Loss\n"
+      GT -> write "Win\n"
+
+
 -- Full function, converst String to Hand data
 ioToHand :: String -> Hand
 ioToHand choice = case map toLower choice of
@@ -39,16 +49,23 @@ ioToHand choice = case map toLower choice of
   "scissors" ->  Scissors
   "paper" ->  Paper
   _ -> Invalid
--- main function that calls specific functions
-playRound :: IO ()
-playRound =
+-- function that calls specific functions,
+-- Basically, read from Input,
+-- Use it as Hand, compare it,
+-- And then,
+-- If its invalid, print invalid, and end,
+-- else, make the play, write the score, and call main again
+playRound :: Hand -> IO ()
+playRound enemy =
   putStrLn "Choose Rock, Scissors or paper" >>
   getLine >>= \choice ->
   if ioToHand choice == Invalid
     then putStrLn "Invalid"
-    else handCompare  (ioToHand choice) >>
+    else handCompare (ioToHand choice) enemy >>
+         writeHandCompare (ioToHand choice) enemy >>
          main
-
+-- said main fn
 main :: IO ()
 main =
-  playRound
+  handOpponent >>= \enemy ->
+  playRound enemy
